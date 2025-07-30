4864196517277e20b54ca184028c70b12c95bb5b
SergeySN84 <Sergsvircuk@mail.ru>
2025-07-30 21:14:28 +0700

    Написание основных модулей для домашнего задания в новой ветке.

--- src/category.py
+++ src/category.py
@@ -1,25 +1,37 @@
 from src.product import Product
 
+
 class Category:
     total_categories = 0
+    product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.__products = products  # Приватное поле
+        self.__products = []
         Category.total_categories += 1
 
+        if products:
+            for product in products:
+                self.add_product(product)
+
     def add_product(self, product):
+
         if not isinstance(product, Product):
-            raise ValueError("Можно добавлять только объекты типа Product.")
+            raise ValueError("Товар с нулевым количеством"
+                             " не может быть добавлен")
+        if product.quantity <= 0:
+            raise ValueError("Товар с нулевым количеством"
+                             " не может быть добавлен")
         self.__products.append(product)
+        Category.product_count += 1
 
     @property
     def products(self):
-        return '\n'.join([str(product) for product in self.__products])
+        return "\n".join(str(product) for product in self.__products)
 
     def __str__(self):
-        total_products = len(self.__products)
-        total_quantity = sum(product.quantity for product in self.__products)
-        return (f"{self.name}, количество продуктов: {total_products} шт.,"
-                f" общее количество: {total_quantity} шт.")
+        total_quantity = sum(p.quantity for p in self.__products)
+        return (f"{self.name}, количество продуктов:"
+                f" {len(self.__products)} шт., "
+                f"общее количество: {total_quantity} шт.")
--- src/product.py
+++ src/product.py
@@ -2,13 +2,43 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена некорректно!")
+        else:
+            self._price = value
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if not isinstance(other, Product):
-            raise TypeError("Можно складывать только объекты типа Product.")
+        if type(self) is not type(other):
+            raise TypeError
         return self.price * self.quantity + other.price * other.quantity
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity,
+                 efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity,
+                 country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
