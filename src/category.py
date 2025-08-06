af786bfc6773b64aca882b136b8749be628ddc77
SergeySN84 <Sergsvircuk@mail.ru>
2025-08-06 22:24:55 +0700

    Создание основных модулей домашней работы category.py и product.py

--- src/category.py
+++ src/category.py
@@ -2,21 +2,20 @@ from src.product import Product
 
 
 class Category:
-    total_categories = 0
+    category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
         self.__products = []
-        Category.total_categories += 1
+        Category.category_count += 1
 
         if products:
             for product in products:
                 self.add_product(product)
 
     def add_product(self, product):
-
         if not isinstance(product, Product):
             raise TypeError("Можно добавлять только объекты класса Product или его наследников")
 
@@ -25,7 +24,6 @@ class Category:
 
         if product.quantity <= 0:
             raise ValueError("Товар с нулевым количеством не может быть добавлен")
-
         self.__products.append(product)
         Category.product_count += 1
 
@@ -35,6 +33,5 @@ class Category:
 
     def __str__(self):
         total_quantity = sum(p.quantity for p in self.__products)
-        return (f"{self.name}, количество продуктов:"
-                f" {len(self.__products)} шт., "
+        return (f"{self.name}, количество продуктов: {len(self.__products)} шт., "
                 f"общее количество: {total_quantity} шт.")
--- src/product.py
+++ src/product.py
@@ -1,9 +1,36 @@
-class Product:
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    @abstractmethod
+    def get_description(self):
+        pass
+
+    @abstractmethod
+    def get_price(self):
+        pass
+
+
+class LogCreationMixin:
+
+    pass
+
+
+class Product(LogCreationMixin, BaseProduct):
+    product_count = 0
+    category_count = 0
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self._price = price
         self.quantity = quantity
+
+
+        print(f"Product('{self.name}', '{self.description}', {self.price}, {self.quantity})")
+
+        if self.quantity > 0:
+            Product.product_count += 1
 
     @property
     def price(self):
@@ -13,8 +40,21 @@ class Product:
     def price(self, value):
         if value <= 0:
             print("Цена введена некорректно!")
+        elif value < self._price:
+            choice = input("Вы уверены, что хотите понизить цену? (y/n): ")
+            if choice.lower() == 'y':
+                self._price = value
+                print("Цена успешно понижена.")
+            else:
+                print("Цена не была изменена.")
         else:
             self._price = value
+
+    def get_description(self):
+        return self.description
+
+    def get_price(self):
+        return self._price
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
@@ -26,19 +66,17 @@ class Product:
 
 
 class Smartphone(Product):
-    def __init__(self, name, description, price, quantity,
-                 efficiency, model, memory, color):
-        super().__init__(name, description, price, quantity)
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
         self.color = color
+        super().__init__(name, description, price, quantity)
 
 
 class LawnGrass(Product):
-    def __init__(self, name, description, price, quantity,
-                 country, germination_period, color):
-        super().__init__(name, description, price, quantity)
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
         self.country = country
         self.germination_period = germination_period
         self.color = color
+        super().__init__(name, description, price, quantity)
